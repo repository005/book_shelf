11f923434c31c5b0f5a81c214738a07a0c376947
Dasha Zabelina <dariaszabelina@gmail.com>
2020-08-28 17:41:53 +0300

    started with server post books

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: tr
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.post('/api/book', (req, res) => {
+  const book = new book(req.body);
+
+  book.save((err,doc) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({
+      post: true,
+      bookId: doc._id
+    })
+  })
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server is running on port:${port}`);

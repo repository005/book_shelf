36decf44b0e1b2f0d06549e354f359e309ff9e35
Ksana <notabeech@gmail.com>
2020-09-08 02:04:28 +0300

    done with jwt token

--- server/server.js
+++ server/server.js
@@ -7,13 +7,22 @@ const config = require('./config/config').get(process.env.NODE_ENV);
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.get('/api/logout', auth, (req,res) => {
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  })
+
+})
 
 app.get('/api/getBook', (req,res) => {
   let id = req.query.id;
@@ -65,6 +74,82 @@ app.delete('/api/delete_book', (req,res) => {
     res.json(true);
   })
 })
+
+app.post('/api/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err,doc) => {
+    if (err) return res.json({
+      success: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      user: doc
+    });
+  })
+})
+
+
+app.post('/api/login', (req,res) => {
+  User.findOne({'email': req.body.email}, (err, user) =>{
+    if (!user) return res.json({
+      isAuth: false,
+      message: 'Auth failed, user not found'
+    })
+
+    if (err) return res.json({
+      isAuth: false,
+      message: 'Auth failed, wrong password'
+    })
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({
+        isAuth: false,
+        message: 'Wrong password'
+      });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res.cookie('auth', user.token).send({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        })
+      })
+
+    })
+  })
+});
+
+app.get('/api/getReviewer', (req,res) => {
+  let id = req.query.id;
+
+  User.findById(id, (err, doc) => {
+    if (err) res.status(400).send(err);
+
+    res.json({
+      name: doc.name,
+      lastname: doc.lastname
+    })
+  })
+})
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) res.status(400).send(err);
+
+    res.status(200).json(users);
+  })
+})
+
+app.get('/api/user_posts', (req,res) => {
+  Book.find({ownerId: req.query.user}).exec((err, docs) => {
+    if (err) return res.status(200).send(err);
+    res.send(docs);
+  })
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {

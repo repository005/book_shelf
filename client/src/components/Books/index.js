08fb2cb511a25e99de7bf9751e4ca7efb7b230cb
Ksana <notabeech@gmail.com>
2020-09-24 02:21:32 +0300

    Merge branch 'master' of https://github.com/repository005/book_shelf into master

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,17 +1,50 @@
 import React, { useEffect } from 'react'
-import { getBookWithReviewer } from '../../actions'
+import { getBookWithReviewer, clearBookWithReviewer } from '../../actions'
 import { connect } from 'react-redux'
 
 const BookView = (props) => {
 
   useEffect(() => {
     props.dispatch(getBookWithReviewer(props.match.params.id));
+    return () => {
+      props.dispatch(clearBookWithReviewer());
+    }
   }, [])
-  console.log(props);
+  
+
+  const renderBook = () => (
+    props.books.book ? 
+    <div className="br_container">
+      <div className="br_header">
+        <h2>{props.books.book.name}</h2>
+        <h5>{props.books.book.author}</h5>
+        <div className="br_reviewer">
+          <span>Review by: </span>{props.books.reviewer.name} {props.books.reviewer.lastname}
+        </div>
+      </div>
+      <div className="br_review">{props.books.book.review}</div>
+      <div className="br_box">
+        <div className="left">
+          <div>
+            <span>Pages:</span> {props.books.book.pages}
+          </div>
+          <div>
+            <span>Price:</span> {props.books.book.price}
+          </div>
+        </div>
+        <div className="right">
+          <span>Rating</span>
+          <div>{props.books.book.rating}/5</div>
+        </div>
+      </div>
+      </div>
+    :
+    null
+  )
 
   return (
     <div>
-      BookView
+      {renderBook()}
     </div>
   )
 }
